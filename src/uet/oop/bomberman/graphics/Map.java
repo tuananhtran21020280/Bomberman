3c9ce8234582132f06c2d23148df14618f3856f1
tuananhtran21020280 <100192298+tuananhtran21020280@users.noreply.github.com>
2022-10-11 04:54:10 +0700

    Update Map.java

--- src/uet/oop/bomberman/graphics/Map.java
+++ src/uet/oop/bomberman/graphics/Map.java
@@ -1,4 +1,22 @@
 package uet.oop.bomberman.graphics;
 
+import uet.oop.bomberman.controller.CollisionManager;
+import uet.oop.bomberman.controller.KeyboardEvent;
+import uet.oop.bomberman.entities.*;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Map {
+    protected List<List<Entity>> map;
+    protected int mapHeight;
+    protected int mapWidth;
+    public List<Entity> entities = new ArrayList<>();
+
+
 }
