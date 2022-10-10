a89ed8d69bc81b94799fa1186dbc447a418d8c45
tuananhtran21020280 <100192298+tuananhtran21020280@users.noreply.github.com>
2022-10-11 04:59:20 +0700

    Update CollisionManager.java

--- src/uet/oop/bomberman/controller/CollisionManager.java
+++ src/uet/oop/bomberman/controller/CollisionManager.java
@@ -18,36 +18,5 @@ public class CollisionManager {
     public CollisionManager(Map map) {
         this.map = map;
     }
-
-    public Map getMap() {
-        return map;
-    }
-
-    public boolean touchObstacle(int x, int y) {
-        Entity topLeft, downLeft, topRight, downRight;
-        topLeft = map.getPosition(x, y);
-        topRight = map.getPosition(x + 20, y);
-        downLeft = map.getPosition(x, y + 29);
-        downRight = map.getPosition(x + 20, y + 29);
-        return topLeft instanceof Obstacle || topRight instanceof Obstacle
-                || downLeft instanceof Obstacle || downRight instanceof Obstacle;
-    }
-
-    public boolean enemyTouchObstacle(int x, int y) {
-        Entity topLeft, downLeft, topRight, downRight;
-        topLeft = map.getPosition(x, y);
-        topRight = map.getPosition(x + 30, y);
-        downLeft = map.getPosition(x, y + 30);
-        downRight = map.getPosition(x + 30, y + 30);
-        return topLeft instanceof Obstacle || topRight instanceof Obstacle
-                || downLeft instanceof Obstacle || downRight instanceof Obstacle;
-    }
-
-    public boolean touchBomb(int x, int y) {
-        return false;
-    }
-
-    public Entity getEntityAt(int x, int y) {
-        return map.getMap().get(y).get(x);
-    }
+    
 }
